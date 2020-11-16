4a2b20738cb8cadcb239234bfa125fe7d0045212
Hidanko <hidan_nemeth@hotmail.com>
2020-11-16 00:22:39 -0300

    correção classificacao.py

--- database/tools/main.py
+++ database/tools/main.py
@@ -1,4 +1,7 @@
-from buscaLarvas import buscar
+from database.tools.buscaLarvas import buscar
+import tensorflow
+
+model = tensorflow.keras.models.load_model('model_saved.h5')
 
 if __name__ == "__main__":
     buscar()
